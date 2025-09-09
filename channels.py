8ad3796484a5dd663584e35d77e758c97628e664
shihaka <att253@yandex.ru>
2025-09-09 11:16:23 +0400

    скорректировал наименование одного канала

--- channels.py
+++ channels.py
@@ -3,7 +3,7 @@
 CHANNELS = [
     ("Excel & Google Sheets", "https://t.me/shihaleev_exg"),
     ("3D-моделирование", "https://t.me/shihaleev_models"),
-    ("Боты на телеграм", "https://t.me/_shihaka_bots"),
+    ("Боты на телеграм", "https://t.me/my_shihaka_bots"),
     ("Мысли и цитаты", "https://t.me/Shihaleev"),
     ("Полезное", "https://t.me/shihaka_poleznoe"),
 ]
